9f6d9d0a7b3b626433a5ccf5b0fc4beb2c448fb0
Andrey <atishin1999@mail.ru>
2023-06-16 13:24:36 +0300

    zadacha1

---
+++ semainar_1/zadacha1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите первое число: ");
+
+double number1 = int.Parse(Console.ReadLine());
+
+Console.Write("введите второе число: ");
+
+double number2 = int.Parse(Console.ReadLine());
+
+if (number1 / number2 == number2)
+
+    Console.Write("Первое число является квадратом второго ");
+
+else
+
+    Console.Write("Первое число не является квадратом второго ");
+
+
