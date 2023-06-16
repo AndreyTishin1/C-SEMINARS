7cffe5d23f6203501b3297edad00fc52f4558fe6
Andrey <atishin1999@mail.ru>
2023-06-16 14:43:00 +0300

    zadacha7

---
+++ semainar_1/zadacha7/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+
+double number = double.Parse(Console.ReadLine());
+
+double number2 = number % 10;
+Console.WriteLine("Посленяя цифра числа: ");
+Console.Write(number2);
+
+
+
