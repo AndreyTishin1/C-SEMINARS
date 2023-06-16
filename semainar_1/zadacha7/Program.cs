e93bbd4801a5fc0b6e0de1262f03853b664e6f3d
Andrey <atishin1999@mail.ru>
2023-06-16 15:14:03 +0300

    zadacha77

--- semainar_1/zadacha7/Program.cs
+++ semainar_1/zadacha7/Program.cs
@@ -2,9 +2,17 @@
 
 double number = double.Parse(Console.ReadLine());
 
-double number2 = number % 10;
+if(number > 99 && number < 1000)
+{
+    double number2 = number % 10;
+
 Console.WriteLine("Посленяя цифра числа: ");
+
 Console.Write(number2);
+}
+else
+Console.WriteLine("Число не трехзначное");
+
 
 
 
