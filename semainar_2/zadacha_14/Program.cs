947b7cb7f8644b686b886c717ba81ded328333da
Andrey <atishin1999@mail.ru>
2023-06-18 17:52:12 +0300

    zadacha14

---
+++ semainar_2/zadacha_14/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int per1 = 7;
+int per2 = 23;
+
+if (number % per1 == 0 && number % per2 == 0)
+{
+    Console.WriteLine($"{number} делится без остатка на {per1} и {per2}");
+}
+else
+{
+    Console.WriteLine("Без остатка не делится");
+}
