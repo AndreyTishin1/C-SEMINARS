a058a605b9289b65935ffe8105e82e76c078bf9e
Andrey <atishin1999@mail.ru>
2023-06-18 17:17:44 +0300

    kratnost

---
+++ semainar_2/zadacha_kratnost/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+int ostatok = 0;
+
+//Console.WriteLine(number1);
+//Console.WriteLine(number2);
+
+if (number1 % number2 == 0)
+{
+    Console.WriteLine($"Число {number1} кратно числу {number2}");
+}
+else
+{
+ostatok = number1 % number2;
+Console.WriteLine($"Число {number1} не кратно числу {number2}, остаток {ostatok}");
+}
