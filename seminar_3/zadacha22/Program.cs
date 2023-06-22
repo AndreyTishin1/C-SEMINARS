aa6c398699874ac49434be1e449c3bb1481bda21
Andrey <atishin1999@mail.ru>
2023-06-22 18:07:50 +0300

    zadacha22

---
+++ seminar_3/zadacha22/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел 
+от 1 до N.*/
+
+Console.Write("Введите число: ");
+double number = int.Parse(Console.ReadLine());
+double index = 1;
+double sqwere = 1;
+while (index <= number)
+{
+    sqwere = Math.Pow(index, 2);
+    Console.Write($"{sqwere}, ");
+    index++;
+}
