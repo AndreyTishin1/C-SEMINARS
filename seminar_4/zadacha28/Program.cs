c6bb16a547851af07ef491b16d98a66a1aa06141
Andrey <atishin1999@mail.ru>
2023-06-25 01:00:35 +0300

    zadacha28

---
+++ seminar_4/zadacha28/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+4 -> 24 
+5 -> 120
+*/
+
+void factorial(int number)
+{
+    int fact = 1;
+    int index = number;
+    while (index > 1)
+    {
+        fact = fact * index;
+        index = index - 1;
+    }
+    Console.WriteLine($"Факториал числа {number} равен {fact}");
+}
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+factorial(number);
