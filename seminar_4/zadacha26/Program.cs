75e7b7ed2b0f14cbce0dee12f99564c4c4e99440
Andrey <atishin1999@mail.ru>
2023-06-24 23:08:17 +0300

    zadacha26

---
+++ seminar_4/zadacha26/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5*/
+
+void metod(int number)
+{
+    int count = 0;  
+    while (number > 0)
+    {  
+        number = number/10;
+        count++;
+    }
+Console.WriteLine($"Количество цифр в числе: {count}");
+}
+Console.WriteLine("Введите число: ");
+int number =  Math.Abs(int.Parse(Console.ReadLine()));
+metod(number);
