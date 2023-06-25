eefe58cf78376ea143fcdbc92dc9a98ce38bf2d3
Andrey <atishin1999@mail.ru>
2023-06-25 17:48:28 +0300

    zadacha33

---
+++ seminar_5/zadacha_33/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+4; массив [6, 7, 19, 345, 3] -> нет
+-3; массив [6, 7, 19, 345, 3] -> да
+*/
+
+
+
+int[] array = new int[7] { 1, 45, -32, -33, 0, 76, -2 };
+Console.WriteLine("Введите число для поиска в массиве: ");
+int number = int.Parse(Console.ReadLine());
+massivPoisk(number);
+
+
+void massivPoisk(int number)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == number)
+        {
+            Console.WriteLine($"Число {number} находится в массиве под индексом {i}");
+            Environment.Exit(0);
+        }
+    }
+    Console.WriteLine("Заданного элемента нет в массиве.");
+}
+
