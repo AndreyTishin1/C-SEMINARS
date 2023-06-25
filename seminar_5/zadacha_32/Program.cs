2e569947b266d6fdddb3f866b3660d52a91478d1
Andrey <atishin1999@mail.ru>
2023-06-25 16:30:39 +0300

    zadacha32

---
+++ seminar_5/zadacha_32/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+[-4, -8, 8, 2] -> [4, 8, -8, -2] 
+*/
+
+int[] array = new int[5] { -45, -8, 7, 57, 8 };
+Console.WriteLine("Исходный масссив: ");
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+Console.WriteLine("Преобразованный массив: ");
+for (int i = 0; i < array.Length; i++)
+{
+   array[i] = array[i]*(-1);
+   Console.Write(array[i] + " ");
+}
