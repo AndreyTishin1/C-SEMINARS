aa89b4cf391585a63c34595da82f9a4b2aaf8f37
Andrey <atishin1999@mail.ru>
2023-06-26 15:57:57 +0300

    zadacha35

--- seminar_5/zadacha_33/Program.cs
+++ seminar_5/zadacha_33/Program.cs
@@ -5,13 +5,7 @@
 
 
 
-int[] array = new int[7] { 1, 45, -32, -33, 0, 76, -2 };
-Console.WriteLine("Введите число для поиска в массиве: ");
-int number = int.Parse(Console.ReadLine());
-massivPoisk(number);
-
-
-void massivPoisk(int number)
+void massivPoisk(int number , int[] array)
 {
     for (int i = 0; i < array.Length; i++)
     {
@@ -24,3 +18,7 @@ void massivPoisk(int number)
     Console.WriteLine("Заданного элемента нет в массиве.");
 }
 
+int[] array = new int[7] { 1, 46, -32, -33, 0, 76, -2 };
+Console.WriteLine("Введите число для поиска в массиве: ");
+int number = int.Parse(Console.ReadLine());
+massivPoisk( number , array );
---
+++ seminar_5/zadacha_35/Program.cs
@@ -0,0 +1,38 @@
+﻿/* Задача 35: Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, значения которых лежат в отрезке [10,99]. 
+Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+[5, 18, 123, 6, 2] -> 1
+[1, 2, 3, 6, 2] -> 0
+[10, 11, 12, 13, 14] -> 5
+*/
+
+void massiv(int[] array, int from, int to)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(from, to);
+        Console.Write(array[i] + " ");
+    }
+
+    Console.WriteLine();
+    Console.WriteLine();
+
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= 10 && array[i] <= 99) 
+        {
+            count = count + 1;
+        }    
+        else
+        {
+            count = count;
+        }
+    }
+    Console.WriteLine($"{count} чисел в диапазоне от {from} до {to} в массиве из 123 элементов ");
+}
+
+Console.WriteLine("Введите диапазон чисел поиска from...to");
+int from = int.Parse(Console.ReadLine());
+int to = int.Parse(Console.ReadLine());
+int[] array = new int[123];
+massiv(array, from, to);
