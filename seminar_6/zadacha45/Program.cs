0257eaa543dc4d7ee6021247d6933f389cae786c
Andrey <atishin1999@mail.ru>
2023-07-03 23:04:09 +0300

    zadacha45

---
+++ seminar_6/zadacha45/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.*/
+
+Console.WriteLine("Введите количество элементов массива:");
+int num = int.Parse(Console.ReadLine());
+int[] array = new int[num];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1, 15);
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine("");
+
+int[] array2 = new int[array.Length];
+for (int j = 0; j < array2.Length; j++)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array2[j] = array[i];
+    }
+Console.Write(array[j] + " ");
+}
+
