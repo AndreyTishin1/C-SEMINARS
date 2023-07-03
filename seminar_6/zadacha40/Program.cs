fd67657b0af2dd519f7b47390b6e63a4103ab3c6
Andrey <atishin1999@mail.ru>
2023-07-03 16:58:38 +0300

    zadacha40

---
+++ seminar_6/zadacha40/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Задача 40:
+ Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
+Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.*/
+
+Console.WriteLine("Введите первое число:");
+double num1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:");
+double num2 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите третье число:");
+double num3 = double.Parse(Console.ReadLine());
+
+if (num1 < num2 + num3 && num2 < num1 + num3 && num3 < num1 + num2)
+{
+    Console.WriteLine("Треугольник с данными сторонами может сушествовать.");
+}
+else
+{
+    Console.WriteLine("Треугольник с данными сторнами существовать не может.");
+}
