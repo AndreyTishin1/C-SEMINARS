b582649f5f65aaa2225d522ac76432e7760703dd
Andrey <atishin1999@mail.ru>
2023-07-03 18:48:03 +0300

    zadacha42

---
+++ seminar_6/zadacha42/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+45 -> 101101
+3  -> 11
+2  -> 10
+*/
+
+Console.WriteLine("Введите десятичное число:");
+int number = int.Parse(Console.ReadLine());
+int number2 = number;
+int ostatok = 0;
+string num = "";
+while (number > 0)
+{
+    ostatok = number % 2;
+    number = number / 2;
+    num = num + ostatok;
+}
+int count = num.Length;
+string dvoichnoe = "";
+while (count > 0)
+{
+    dvoichnoe = dvoichnoe + num[count - 1];
+    count--;
+}
+Console.WriteLine($"Число {number2} в двоичной системе равно {dvoichnoe}");
