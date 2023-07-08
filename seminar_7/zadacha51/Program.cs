e12713b871a77d7c984588c627ea417e3b8c2565
Andrey <atishin1999@mail.ru>
2023-07-08 17:14:44 +0300

    zadacha51

---
+++ seminar_7/zadacha51/Program.cs
@@ -0,0 +1,36 @@
+﻿/* задайте двумерный массив
+найдите сумму элементов находящихся на главной диагонали.*/
+
+Console.WriteLine("Введите количество строк массива:");
+int strok = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов массива: ");
+int stolb = int.Parse(Console.ReadLine());
+int[,] array = new int[strok, stolb];
+Console.WriteLine();
+int rezult = 0;
+
+for (int i = 0; i < strok; i++)
+{
+    for (int j = 0; j < stolb; j++)
+    {
+        array[i, j] = new Random().Next(10, 51);
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+for (int i = 0; i < strok; i++)
+{
+    for (int j = 0; j < stolb; j++)
+    {
+        if (i == j)
+        {
+            rezult = rezult + array[i, j];
+        }
+        else
+        {
+            rezult = rezult;
+        }
+    }
+}
+Console.WriteLine(rezult);
