646f377f617d31147871a2de7a442270e5e91c1e
Andrey <atishin1999@mail.ru>
2023-07-08 16:42:57 +0300

    zadacha49

---
+++ seminar_7/zadacha49/Program.cs
@@ -0,0 +1,39 @@
+﻿/* задайте двумерный массив
+найдите элементы, у которых оба индекса четные и замените эти элементы на их квадраты*/
+
+Console.WriteLine("Введите количество строк массива:");
+int strok = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов массива:");
+int stolb = int.Parse(Console.ReadLine());
+int[,] array = new int[strok, stolb];
+Console.WriteLine();
+Console.WriteLine();
+
+for (int i = 0; i < strok; i++)
+{
+    for (int j = 0; j < stolb; j++)
+    {
+        array[i, j] = new Random().Next(10, 51);
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+for (int i = 0; i < strok; i++)
+{
+    for (int j = 0; j < stolb; j++)
+    {
+        if (i < 2 || i % 2 > 0 || j < 2 || j % 2 > 0)
+        {
+            array[i, j] = array[i, j];
+            Console.Write(array[i, j] + " ");
+        }
+        else
+        {
+            array[i, j] = array[i, j] * array[i, j];
+            Console.Write(array[i, j] + " ");
+        }
+    }
+    Console.WriteLine();
+}
