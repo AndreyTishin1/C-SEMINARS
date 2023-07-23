162244da1312a50ad9da3be0fe00659d4601595f
Andrey <atishin1999@mail.ru>
2023-07-23 19:34:13 +0300

    zadacha 63

---
+++ seminar_9/zadacha63/Program.cs
@@ -0,0 +1,25 @@
+﻿/*int GetFact(int number)
+{
+    if (number == 1) return 1;
+
+    int factor = number * GetFact(number - 1);
+    return factor;
+}
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int factorial = GetFact(number);
+Console.WriteLine($"Факториал {number} равен {factorial}");*/
+
+
+int GetPosl(int n)
+{
+    if (n == 0) return 1;
+    int posled = 1 + GetPosl(n - 1);
+    Console.Write(n + " ");
+    return posled;
+}
+
+Console.WriteLine("Введите число N: ");
+int number = int.Parse(Console.ReadLine());
+GetPosl(number);
